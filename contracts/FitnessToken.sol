0d3bcc74f9fa37bb9e6db81a488192cbc46173bb
Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-12-02 19:31:58 -0500

    Updated openzeppelin and all version numbers of solidity - compiled the contracts

--- contracts/FitnessToken.sol
+++ contracts/FitnessToken.sol
@@ -1,10 +1,9 @@
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FitnessToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Fitness Token", "SWOLE") public {
         _mint(msg.sender, initialSupply);
-    _setupDecimals(0);
     }
 }
