83f318774263c992a02249055c2d1896b498f4d7
Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-12-02 20:18:22 -0500

    KYC contract

--- migrations/2_deployContracts.js
+++ migrations/2_deployContracts.js
@@ -1,5 +1,7 @@
 const FitnessToken = artifacts.require("./FitnessToken.sol");
+const KYCContract = artifacts.require("./KYC.sol");
 
 module.exports = async function (deployer) {
     await deployer.deploy(FitnessToken, 1000000000);
+    await deployer.deploy(KYCContract);
 };
