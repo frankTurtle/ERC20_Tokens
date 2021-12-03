ff93d4bee7587976ed135d577226da7f9323963b
Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-12-02 20:01:38 -0500

    Couple more unit tests

--- test/FitnessToken.test.js
+++ test/FitnessToken.test.js
@@ -18,4 +18,27 @@ contract("SWOLE Token Test", async accounts => {
 
         await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
     });
+
+    it("I can send tokens from Account 1 to Account 2", async () => {
+        const sendTokens = 1;
+        const instance = await Token.deployed();
+        const totalSupply = await instance.totalSupply();
+
+        await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
+        await expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
+        await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
+        await expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+    });
+
+
+    it("It's not possible to send more tokens than account 1 has", async () => {
+        const instance = await Token.deployed();
+        const balanceOfAccount = await instance.balanceOf(initialHolder);
+
+        await expect(instance.transfer(recipient, new BN(balanceOfAccount + 1))).to.eventually.be.rejected;
+
+        //check if the balance is still the same
+        await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(balanceOfAccount);
+
+    });
 });
