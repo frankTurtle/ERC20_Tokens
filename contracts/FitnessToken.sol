121b70b7cc416be6e3cfc7f541678166179de5f6
Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-12-02 19:22:57 -0500

    Renamed token to SWOLE

--- contracts/FitnessToken.sol
+++ contracts/FitnessToken.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FitnessToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Fitness Token", "FIT") public {
+    constructor(uint256 initialSupply) ERC20("Fitness Token", "SWOLE") public {
         _mint(msg.sender, initialSupply);
     _setupDecimals(0);
     }
